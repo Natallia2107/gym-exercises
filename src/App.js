50d3710ba8aae86db372fcae33594a815908d453
Natallia2107 <105502657+Natallia2107@users.noreply.github.com>
2023-02-15 14:58:30 +0100

    created basic structure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import { Box } from '@mui/material'
+import { Home, ExerciseDetail } from './pages'
+import { Navbar, Footer } from './components'
 
 function App() {
-  return (
-    <div className="App">
-      hello app
-    </div>
-  );
+	return (
+		<Box width='400px' sx={{ width: { xl: '1488px' } }} m='auto'>
+			<Navbar />
+			<Routes>
+				<Route path='/' element={<Home />} />
+				<Route path='/exercise/:id' element={<ExerciseDetail />} />
+			</Routes>
+			<Footer />
+		</Box>
+	)
 }
 
-export default App;
+export default App
