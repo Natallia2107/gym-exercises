00d08638b186d394d37d5d3a6977bd319197a4c1
Natallia2107 <105502657+Natallia2107@users.noreply.github.com>
2023-02-17 16:18:49 +0100

    created some component fro exercise detail component and export them for index.js and import for exercise detail component

---
+++ src/components/ExerciseVideos.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ExerciseVideos = () => {
+  return (
+    <div>
+      Exercise Viedos
+    </div>
+  )
+}
+
+export default ExerciseVideos
--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { Box } from '@mui/material'
 import { exerciseOptions, fetchData } from '../utils/fetchData'
+import { Detail, ExerciseVideos, SimilarExercises } from '../components'
 
 const ExerciseDetail = () => {
 	return (
