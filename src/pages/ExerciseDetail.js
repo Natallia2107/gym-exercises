7310f0a7a798287e9a96d04bdcbba894c7502c98
Natallia2107 <105502657+Natallia2107@users.noreply.github.com>
2023-02-20 13:25:14 +0100

    styled exercise detail component

--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -2,26 +2,32 @@ import React from 'react'
 import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { Box } from '@mui/material'
-import { exerciseOptions, fetchData } from '../utils/fetchData'
+import { exerciseOptions, fetchData, youtubeOptions } from '../utils/fetchData'
 import { Detail, ExerciseVideos, SimilarExercises } from '../components'
 
 const ExerciseDetail = () => {
-  const [ExerciseDetail, setExerciseDetail] = useState({})
-  const { id } = useParams()
-  useEffect(() => {
-    const fetchExercisesData = async () => {
-      const exerciseBdUrl = 'https://exercisedb.p.rapidapi.com'
-      const youtubeSearchUrl = 'https://youtube-search-and-download.p.rapidapi.com'
-      const exerciseDetailData = await fetchData(`${exerciseBdUrl}/exercises/${id}`, exerciseOptions)
-      setExerciseDetail(exerciseDetailData)
-    }
-    fetchExercisesData()
-  }, [id])
+	const [ExerciseDetail, setExerciseDetail] = useState({})
+  const [exerciseVideos, setExerciseVideos] = useState([])
+	const { id } = useParams()
+	useEffect(() => {
+		const fetchExercisesData = async () => {
+			const exerciseDbUrl = 'https://exercisedb.p.rapidapi.com'
+			const youtubeSearchUrl = 'https://youtube-search-and-download.p.rapidapi.com'
+
+			const exerciseDetailData = await fetchData(`${exerciseDbUrl}/exercises/exercise/${id}`, exerciseOptions)
+			setExerciseDetail(exerciseDetailData)
+
+      const exerciseVideosData = await fetchData(`${youtubeSearchUrl}/search?query=${exerciseDetailData.name}`, youtubeOptions)
+      setExerciseVideos(exerciseVideosData)
+
+		}
+		fetchExercisesData()
+	}, [id])
 
 	return (
 		<Box>
 			<Detail ExerciseDetail={ExerciseDetail} />
-			<ExerciseVideos />
+			<ExerciseVideos exerciseVideos={exerciseVideos} name={exerciseDetail.name} />
 			<SimilarExercises />
 		</Box>
 	)
